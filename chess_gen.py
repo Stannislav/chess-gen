37121bcc2b9924e850e2f31f50f7031868c767d2
Stanislav Schmidt <Stannislav@users.noreply.github.com>
2025-02-22 19:34:42 +0100

    Bump version (#6)

--- chess_gen.py
+++ chess_gen.py
@@ -14,7 +14,7 @@ from rich.columns import Columns
 from rich.panel import Panel
 from rich.table import Table
 
-__version__ = "1.1.0"
+__version__ = "1.2.0"
 
 WHITE_PAWN = Piece.from_symbol("P")
 BLACK_PAWN = Piece.from_symbol("p")
